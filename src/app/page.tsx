55efa3d7565b25f51539052196ba7c32a7814b8d
kennytobiloba <olaatunbikehinde@gmail.com>
2024-10-16 10:07:40 +0100

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import HomePage from '@/components/layounts/HomePage';
-import Banner from "@/components/layounts/Banner"
+import  Banner from "@/components/layounts/Banner"
 import Group from "@/components/layounts/Group"
 import ElectionInfo from '@/components/layounts/ElectionInfo';
 import Services from '@/components/layounts/Services';
--- src/components/layounts/Banner.jsx
+++ src/components/layounts/Banner.jsx
@@ -3,8 +3,8 @@ import React from "react";
 const Banner = () => {
   return (
     <div>
-      <div className="banner-bottom1 mt-1">
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-1">
+      <div className="banner-bottom1 mt-8">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
           {/* Campaign */}
           <div className="bg-slate-200 px-6 py-10 md:py-16 lg:py-12 border-4
            border-t-purple-600  transition duration-300 hover:border-t-white hover:bg-[#c05fe6]">
@@ -20,7 +20,7 @@ const Banner = () => {
                 <h3 className="text-[20px] md:text-[22px] lg:text-[24px]  mb-2">
                   Campaign
                 </h3>
-                <p className="text-gray-600 text-sm md:text-base lg:text-lg">
+                <p className="text-black text-sm md:text-base lg:text-lg">
                   Lorem Ipsum is therefore always free.
                 </p>
               </div>
@@ -39,10 +39,10 @@ const Banner = () => {
                 <span className="mb-2">
                   <i className="fa-solid text-[18px] fa-user"></i>
                 </span>
-                <h3 className="text-[20px] md:text-[22px] text-red-200 lg:text-[24px]  mb-2">
+                <h3 className="text-[20px] md:text-[22px] text-red-black lg:text-[24px]  mb-2">
                   Accessibility
                 </h3>
-                <p className="text-gray-600 text-sm md:text-base lg:text-lg">
+                <p className="text-black text-sm md:text-base lg:text-lg">
                   Lorem Ipsum is therefore always free.
                 </p>
               </div>
@@ -64,7 +64,7 @@ const Banner = () => {
                 <h3 className="text-[20px] md:text-[22px] lg:text-[24px] mb-2">
                   Calendar
                 </h3>
-                <p className="text-gray-600 text-sm md:text-base lg:text-lg">
+                <p className="text-gray-black text-sm md:text-base lg:text-lg">
                   Lorem Ipsum is therefore always free.
                 </p>
               </div>
